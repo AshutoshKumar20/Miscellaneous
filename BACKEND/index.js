1e705ded4bc04d4fcb8b8460237e5305e8401f0b
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-02-20 12:08:38 +0530

    Learnt to handle POST requests

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+// If request is urlencoded, then express automatically parses it and understand it
+app.use(express.json());
+//If we are sending JSON data, then we write this line
+
 app.get("/register", (req, res) => {
     let { user, password } = req.query;
     res.send(`Standard GET response. Welcome ${user}!!!`)
 });
 
 app.post("/register", (req, res) => {
-
-    res.send(`Standard POST response.`);
+    let { user, password } = req.body;
+    res.send(`Standard POST response. Welcome ${user}!!!`);
 });
 
 app.listen(port, () => {
